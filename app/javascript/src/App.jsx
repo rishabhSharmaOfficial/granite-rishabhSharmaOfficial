c4733a7309fbbb2166080eb4609e6f6aeacd02db
Rishabh Sharma <risharma654@gmail.com>
2022-08-31 14:27:04 +0530

    Set up react environment

---
+++ app/javascript/src/App.jsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from "react";
+import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import { setAuthHeaders } from "apis/axios";
+// previous code if any
+import { initializeLogger } from "common/logger";
+
+const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    /*eslint no-undef: "off"*/
+    initializeLogger();
+    setAuthHeaders(setLoading);
+    // logger.info("Never use console.log");
+    // logger.error("Never use console.error");
+  }, []);
+
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" render={() => <div>Home</div>} />
+        <Route exact path="/about" render={() => <div>About</div>} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default App;
+
+// const App = () => {
+//   const [loading, setLoading] = useState(true);
+//   // previous code if any
+
+//   useEffect(() => {
+//     setAuthHeaders(setLoading);
+//   }, []);
+
+//   if (loading) {
+//     return <h1>Loading...</h1>;
+//   }
+
+//   // previous code without changes
+// };
+
+
+// import React, { useEffect, useState } from "react";
+// import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+
+// const App = () => {
+//   return (
+//     <Router>
+//       <Switch>
+//         <Route exact path="/" render={() => <div>Home</div>} />
+//         <Route exact path="/about" render={() => <div>About</div>} />
+//       </Switch>
+//     </Router>
+//   );
+// };
+
+// export default App;
