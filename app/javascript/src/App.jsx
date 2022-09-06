9fbeb106b28a6eb25ce08716ae114492f86b7c73
Rishabh Sharma <risharma654@gmail.com>
2022-09-06 14:28:26 +0530

    Created Toastr component and added Axios interceptors

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,73 +1,40 @@
 import React, { useEffect, useState } from "react";
 
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
-import { setAuthHeaders } from "apis/axios";
-// previous code if any
+import { registerIntercepts, setAuthHeaders } from "apis/axios";
 import { initializeLogger } from "common/logger";
 import Dashboard from "components/Dashboard";
+import PageLoader from "components/PageLoader";
 import CreateTask from "components/Tasks/Create";
-
-import NavBar from "./components/NavBar";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    /*eslint no-undef: "off"*/
     initializeLogger();
+    registerIntercepts();
     setAuthHeaders(setLoading);
-    // logger.info("Never use console.log");
-    // logger.error("Never use console.error");
   }, []);
 
   if (loading) {
-    return <h1>Loading...</h1>;
+    return (
+      <div className="h-screen">
+        <PageLoader />
+      </div>
+    );
   }
 
   return (
     <Router>
-      <Route exact component={NavBar} path="/" />
+      <ToastContainer />
       <Switch>
-        <Route exact path="/" render={() => <div>Home</div>} />
-        <Route exact path="/about" render={() => <div>About</div>} />
-        <Route exact component={Dashboard} path="/dashboard" />
         <Route exact component={CreateTask} path="/tasks/create" />
-        {/* <Route exact path="/dashboard" component={Dashboard} /> */}
+        <Route exact component={Dashboard} path="/dashboard" />
       </Switch>
     </Router>
   );
 };
 
 export default App;
-
-// const App = () => {
-//   const [loading, setLoading] = useState(true);
-//   // previous code if any
-
-//   useEffect(() => {
-//     setAuthHeaders(setLoading);
-//   }, []);
-
-//   if (loading) {
-//     return <h1>Loading...</h1>;
-//   }
-
-//   // previous code without changes
-// };
-
-// import React, { useEffect, useState } from "react";
-// import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
-
-// const App = () => {
-//   return (
-//     <Router>
-//       <Switch>
-//         <Route exact path="/" render={() => <div>Home</div>} />
-//         <Route exact path="/about" render={() => <div>About</div>} />
-//       </Switch>
-//     </Router>
-//   );
-// };
-
-// export default App;
