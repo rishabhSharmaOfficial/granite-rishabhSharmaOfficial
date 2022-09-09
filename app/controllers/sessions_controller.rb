0d1606385579cea7199b936aff5aa177c2248ddd
Rishabh Sharma <risharma654@gmail.com>
2022-09-09 16:58:50 +0530

    Added login feature

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class SessionsController < ApplicationController
+  skip_before_action :authenticate_user_using_x_auth_token
+
+  def create
+    @user = User.find_by!(email: login_params[:email].downcase)
+    unless @user.authenticate(login_params[:password])
+      respond_with_error(t("session.incorrect_credentials"), :unauthorized)
+      # respond_with_error("Incorrect credentials, try again.", :unauthorized)
+    end
+  end
+
+  private
+
+    def login_params
+      params.require(:login).permit(:email, :password)
+    end
+end
